c4ccedf4e3d5f3c929fa597057965354569f07ac
clr-team <mhippely@vmware.com>
2018-08-31 18:32:09 +0000

    Clarity Build: [NG] Schematics update for importing deprecated non-Clr classes http://github.com/vmware/clarity/commit/6aa8df8eb8a888f8c181f849c874f1c9b95c7bc5:

--- schematics/update/update-0.13.0.js
+++ schematics/update/update-0.13.0.js
@@ -18,6 +18,38 @@ Start changes for Clarity 0.13.0
             }
             messageFlags[message] = true;
         }
+        function getNewClassname(oldClassname) {
+            switch (oldClassname) {
+                case 'Comparator':
+                    return 'ClrDatagridComparatorInterface';
+                case 'Filter':
+                    return 'ClrDatagridFilterInterface';
+                case 'SortOrder':
+                    return 'ClrDatagridSortOrder';
+                case 'State':
+                    return 'ClrDatagridStateInterface';
+                case 'StringFilter':
+                    return 'ClrDatagridStringFilterInterface';
+                case 'Checkbox':
+                    return 'ClrCheckboxDeprecated';
+                case 'DatagridHideableColumnDirective':
+                    return 'ClrDatagridHideableColumn';
+                case 'NavLevelDirective':
+                    return 'ClrNavLevel';
+                case 'TabLinkDirective':
+                    return 'clrTabLink';
+                case 'WizardPageButtonsDirective':
+                    return 'ClrWizardPageButtons';
+                case 'WizardPageHeaderActionsDirective':
+                    return 'ClrWizardPageHeaderActions';
+                case 'WizardPageNavTitleDirective':
+                    return 'ClrWizardPageNavTitle';
+                case 'WizardPageTitleDirective':
+                    return 'ClrWizardPageTitle';
+                default:
+                    return 'Clr' + oldClassname;
+            }
+        }
         // Recurse through the directory scanning and changing text as we go
         function replaceContent(path) {
             const dir = tree.getDir(path);
@@ -40,6 +72,32 @@ Start changes for Clarity 0.13.0
                         updated = updated.replace('ClarityModule.forChild()', 'ClarityModule');
                         updated = updated.replace('ClarityModule.forRoot()', 'ClarityModule');
                     }
+                    // Check for deprecated classes and update if possible
+                    const reg = /import\s*{([^}]*)}\s*from\s*['"]@clr\/angular['"]/g;
+                    let result = reg.exec(updated);
+                    while (result !== null) {
+                        // We are using Cl instead of Clr in the regex for simplicity, because we export
+                        // `ClarityModule,` which would be covered by this regex.
+                        const classRegex = /\b(?!Cl)\w+\b/g;
+                        let importedClass = classRegex.exec(result[1]);
+                        while (importedClass !== null) {
+                            // prefix the directives that don't start with `CLR_`
+                            if (importedClass[0].search(/\b(?!CLR_)\w+_DIRECTIVES\b/g) > -1) {
+                                const re = new RegExp(importedClass[0], 'g');
+                                updated = updated.replace(re, 'CLR_$&');
+                            }
+                            else if (importedClass[0].search(/menuPositions/g) > -1) {
+                                logMessage('error', 'ACTION REQUIRED: Clarity has removed `menuPositions`, you need to either remove all uses or follow migration instructions.', filePath, '0.13.0-beta.2');
+                            }
+                            else {
+                                const re = new RegExp(importedClass[0], 'g');
+                                updated = updated.replace(re, getNewClassname(importedClass[0]));
+                            }
+                            // update to next class name for the while loop
+                            importedClass = classRegex.exec(result[1]);
+                        }
+                        result = reg.exec(updated);
+                    }
                     // Alert to using ClrCodeHighlight
                     if (updated.search(/ClrCodeHighlight/g) > -1 ||
                         updated.search(/clr-code-highlight/g) > -1 ||
@@ -55,13 +113,13 @@ Start changes for Clarity 0.13.0
                     if (updated.search(/clrType=['|"]selectAll|ok['|"]/g) > -1) {
                         logMessage('warn', 'WARNING: Clarity has removed the ok button (clrType="ok") from the column hide/show panel in datagrid. Any custom <clr-dg-column-toggle-button> component will now default to clrType="selectAll" without needing the attribute. Please remove any custom ok button in the column hide/show panel, and also remove the clrType attributes for buttons with clrType="selectAll"', filePath, '0.13.0-beta.2');
                     }
-                    // Alert to using deprecated icons
-                    if (updated.search(/shape=['"](angle-double|bar-chart|collapse|line-chart|wand)['"]/g) > -1) {
-                        logMessage('info', 'UPDATED: The following icons have been deprecated in its respective set and have been moved to another set: \n`angle-double`, `bar-chart`, `collapse`, `line-chart`, `wand`\nPlease check that they are being imported from the correct set.', filePath, '0.13.0-beta.2');
-                    }
                     // Alert to using bootstrap's push / pull in Clarity grid
                     if (updated.search(/(push|pull)-(xs|sm|md|lg|xl)-([0-9])/g) > -1) {
                         logMessage('error', 'ACTION REQUIRED: Clarity has removed push and pull support in its grid system, you need either remove all uses or follow migration instructions.', filePath, '0.13.0-beta.2');
+                    }
+                    // Alert to using deprecated icons
+                    if (updated.search(/shape=['"](angle-double|bar-chart|collapse|line-chart|wand)['"]/g) > -1) {
+                        logMessage('info', 'INFO: The following icons have been deprecated in its respective set and have been moved to another set: \n`angle-double`, `bar-chart`, `collapse`, `line-chart`, `wand`\nPlease check that they are being imported from the correct set.', filePath, '0.13.0-rc.1');
                     }
                     if (content !== updated) {
                         tree.overwrite(filePath, updated);
