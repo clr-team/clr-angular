d6f23caa666b0c4412e3c8ba979adc14f775c116
clr-team <mhippely@vmware.com>
2018-11-21 04:40:18 +0000

    Clarity Build: [icons] Icon improvements and handle loading for Angular applications automatically (#2867) http://github.com/vmware/clarity/commit/838edc403cd1e5fded65f2efdfdce944ea13d1d5:

--- icon/icon.d.ts
+++ icon/icon.d.ts
@@ -1,2 +1,8 @@
+import { ElementRef, Renderer2 } from '@angular/core';
 export declare class ClrIconCustomTag {
+    private el;
+    private renderer;
+    shape: string;
+    constructor(el: ElementRef, renderer: Renderer2);
+    ngOnInit(): void;
 }
